ac4a13f71badff0b19ddffd1b4b35f997874e4e3
Wesley Prado <wprado1424@gmail.com>
2022-09-11 19:18:48 -0300

    test: ensure SignUp route returns an account on success ✅

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,7 @@
-import { Router, Response } from 'express'
+import { Router, Request, Response } from 'express'
 
 export default (router: Router): void => {
-  router.post('/signup', (_, res: Response) => {
+  router.post('/signup', (req: Request, res: Response) => {
     res.json({ ok: true })
   })
 }

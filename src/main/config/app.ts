a8f2e13dce0f8a16d43e8114e1b97eb680a46090
Wesley Prado <wprado1424@gmail.com>
2022-09-11 11:35:53 -0300

    feat: ensure cors is enabled ✨

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,4 +1,4 @@
-import * as express from 'express'
+import express = require('express')
 import setupMiddlewares from './middlewares'
 
 const app = express()
---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,20 @@
+import app from '../config/app'
+import request = require('supertest')
+
+const routes = {
+  CORS: '/test_cors'
+}
+
+describe('CORS middleware', () => {
+  it('must allow all origin, header and methods', async () => {
+    app.get(routes.CORS, (req, res) => {
+      res.status(200).send()
+    })
+
+    await request(app)
+      .get(routes.CORS)
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-headers', '*')
+      .expect('access-control-allow-methods', '*')
+  })
+})

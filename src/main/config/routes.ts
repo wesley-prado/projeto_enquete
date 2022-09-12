bd8a48bab4366d454d61349ab09726ff2c2b6ce2
Wesley Prado <wprado1424@gmail.com>
2022-09-12 20:59:09 -0300

    refactor: use native node libs instead fast-glob ♻️

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,9 +1,11 @@
 import { Express, Router } from 'express'
-import * as fg from 'fast-glob'
+import * as fs from 'fs'
+import * as path from 'node:path'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
+  const routesPath = path.join(__dirname, '../routes')
 
-  fg.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
+  fs.readdirSync(routesPath).filter(file => file.endsWith('-routes.ts')).map(async file => (await import(`${routesPath}/${file}`)).default(router))
 }

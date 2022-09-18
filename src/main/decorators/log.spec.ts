5fd6a77b1894fdc4e4420fdc7882c6e5c88d6e6e
Wesley Prado <wprado1424@gmail.com>
2022-09-18 17:48:21 -0300

    test: ensure logControllerDecorator calls controller with correct value ✨

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,51 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+import { LogControllerDecorator } from './log'
+
+interface MakeSutType {
+  sut: Controller
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        body: '',
+        statusCode: 200
+      }
+
+      return await Promise.resolve(httpResponse)
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeSut = (): MakeSutType => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+describe('LogControllerDecorator', () => {
+  it('Should call controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'Wesley Prado',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    await sut.handle(httpRequest)
+
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
